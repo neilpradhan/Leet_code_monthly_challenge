854314c9dc5d630dd8e615dd8c00bc7c972fabf8
Neil Pradhan <34929271+neilpradhan@users.noreply.github.com>
2020-04-18 13:44:08 +0200

    Update Minimum_Path_sum.cpp

--- Minimum_Path_sum.cpp
+++ Minimum_Path_sum.cpp
@@ -1,19 +1,19 @@
 class Solution {
 private:
-    int dfs(int i, int j,vector<vector<int>>& grid,int r,int c){
+//     int dfs(int i, int j,vector<vector<int>>& grid,int r,int c){
         
-        if (i==r-1 && j==c-1){
-            return grid[i][j];
-        }
+//         if (i==r-1 && j==c-1){
+//             return grid[i][j];
+//         }
         
-        if (i<0 || j<0 || i>=r || j>=c){return 0;}
+//         if (i<0 || j<0 || i>=r || j>=c){return 0;}
         
-        int right = dfs(i,j+1,grid,r,c);
-        int down  = dfs(i+1,j,grid,r,c);
+//         int right = dfs(i,j+1,grid,r,c);
+//         int down  = dfs(i+1,j,grid,r,c);
         
-        return std::min(right,down);
+//         return std::min(right,down);
         
-    }
+//     }
     
     
     
@@ -23,14 +23,24 @@ public:
         int rows  = grid.size();
         int cols  = grid[0].size();
         
-        int ans = INT_MAX;
-        // for(int i=0;i<rows;i++){
-        //     for (int j=0;j<cols;j++){
-                ans = std::min(ans,dfs(0,0,grid,rows,cols));
-                
-// '            }
-//         }'
+        int dp[rows][cols];
+            dp[0][0] = grid[0][0];
+        for(int i =1;i<rows;i++){
+            dp[i][0] = dp[i-1][0] + grid[i][0];
+        }
+        for(int j =1;j<cols;j++){
+            dp[0][j] = dp[0][j-1] + grid[0][j];
+        }
         
-        return dfs(0,0,grid,rows,cols);
+        for(int i =1;i<rows;i++){  
+            for(int j =1;j<cols;j++){
+            
+            dp[i][j] = min(dp[i][j-1],dp[i-1][j]) + grid[i][j];
+        
+            }
+         }     
+        return dp[rows-1][cols-1];
+        
+
     }
 };
