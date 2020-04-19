5259b25c1e4121c7063f611d667461a6a1954db0
Neil Pradhan <34929271+neilpradhan@users.noreply.github.com>
2020-04-19 13:42:50 +0200

    Create search_in_rotated_sorted_array.cpp

---
+++ search_in_rotated_sorted_array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        unordered_map<int,int>mp;
+        
+        for (int i=0;i<nums.size();i++){
+            mp[nums[i]] = i;
+        }
+        
+        if (mp.find(target)!=mp.end()){
+            return mp[target];
+        }else{
+            return -1;
+        }
+        
+        
+    }
+};
