0fc5ca7f3a5218e3a65035fd2afc583dd453b778
Neil Pradhan <34929271+neilpradhan@users.noreply.github.com>
2020-05-24 10:06:22 +0200

    Create Count_Square_Submatrices_with_All_Ones.cpp

---
+++ May_challenge/Count_Square_Submatrices_with_All_Ones.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+        
+        int count = 0;
+        for (int i = 0;i<matrix.size();i++){
+            for(int j = 0;j<matrix[0].size();j++){
+                
+            }
+        }
+        
+        return count;
+        
+        
+    }
+};
